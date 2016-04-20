725dec30c8cb47b716eee5e18a7ce86ee534ee55
root <dsteil@harding.edu>
2016-04-20 17:55:42 -0400

    fixed add to return json

--- api/math/add.js
+++ api/math/add.js
@@ -3,7 +3,7 @@ var router = require('express').Router()
 router.get('/math/add/:x/:y', function (req, res, next) {
   var x = req.params.x;
   var y = req.params.y;
-  res.send(x+y);
+  res.json({result:parseInt(x)+parseInt(y)});
 })
 
 
